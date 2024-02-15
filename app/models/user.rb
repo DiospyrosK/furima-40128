c37b618d1763850cff5c2cf291c3c997d4f49f42
DiospyrosK <3.dsoul226@gmail.com>
2024-02-16 01:48:31 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, :email, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { minimum: 6 }, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :encrypted_password, presence: true, confirmation: true, format: { with: PASSWORD_REGEX }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :first_name
     validates :last_name
