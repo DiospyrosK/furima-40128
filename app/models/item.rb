dfe1145e632b003a444b83b107bac7e3b5f0fc0a
DiospyrosK <3.dsoul226@gmail.com>
2024-02-18 17:22:53 +0900

    item.rbの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
     validates :shipping_price_id
     validates :shipping_area_id
     validates :shipping_date_id
-    validates :price, numericality: { in: 300..9,999,999 }
+    validates :price, numericality: { in: 300..9_999_999 }
   end
 
   with_options numericality: { other_than: 1 } do
@@ -21,6 +21,10 @@ class Item < ApplicationRecord
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :shipping_area, :shipping_price, :shipping_date
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_area
+  belongs_to :shipping_price
+  belongs_to :shipping_date
   belongs_to :user
 end
