2db97d44f97f22692f7a789f61c1ef70bcdde121
DiospyrosK <3.dsoul226@gmail.com>
2024-03-14 06:32:34 +0900

    order_destinationの編集

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,12 +1,12 @@
 class OrderDestination
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :street_address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :post_code, :shipping_area_id, :city, :street_address, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :user_id
     validates :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :shipping_area_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :shipping_area_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :street_address
     validates :phone_number
