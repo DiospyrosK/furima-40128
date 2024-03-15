338039704568ec56d1142627dbfa7e696f476605
DiospyrosK <3.dsoul226@gmail.com>
2024-03-16 06:38:20 +0900

    Pay.jpの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
   def create
     @order_destination = OrderDestination.new(order_params)
     if @order_destination.valid?
+      pay_item
       @order_destination.save
       redirect_to root_path
     else
@@ -19,7 +20,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_destination).permit(:post_code, :shipping_area_id, :city, :street_address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_destination).permit(:post_code, :shipping_area_id, :city, :street_address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
   def set_item
--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,6 +1,6 @@
 class OrderDestination
   include ActiveModel::Model
-  attr_accessor :post_code, :shipping_area_id, :city, :street_address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :post_code, :shipping_area_id, :city, :street_address, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :user_id
@@ -9,7 +9,8 @@ class OrderDestination
     validates :shipping_area_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :street_address
-    validates :phone_number, format: {with: /A0\d{9,10}\z/, message: "is invalid"}
+    validates :phone_number, format: {with: /\A0\d{9,10}\z/, message: "is invalid"}
+    validates :token
   end
 
   def save
